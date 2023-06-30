7f0b14a9a136ace1cc0d2f0c570b3712908f4673
Nikita Anand <51712876+nikanand04@users.noreply.github.com>
2023-06-30 14:08:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,8 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/nikanand04/s3-webapp/aws"
+  #source  = "app.terraform.io/nikanand04/s3-webapp/aws"
+  source  = "app.terraform.io/hashicorp_demos/s3-webapp/aws"
   name   = var.name
   region = var.region
   prefix = var.prefix
